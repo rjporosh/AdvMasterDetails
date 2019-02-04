6d63b973789942649d9607e42aab84dae33e2ced
rjporosh <porosh199404232@gmail.com>
2019-02-04 22:27:01 +0600

    First And Final Commit All Working Fine

---
+++ AdvMasterDetails/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvMasterDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+        public JsonResult GetProductCategories()
+        {
+            List<Category> categories;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                categories = dc.Categories.OrderBy(a => a.CategortyName).ToList();
+            }
+            return new JsonResult { Data = categories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        public JsonResult GetProducts(int categoryID)
+        {
+            List<Product> products = new List<Product>();
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                products = dc.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
+            }
+            return new JsonResult { Data = products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        [HttpPost]
+        public JsonResult Save(OrderMaster order)
+        {
+            bool status = false;
+            DateTime dateOrg;
+            //var isValidDate = DateTime.TryParseExact(order.OrderDate.ToString(), "mm-dd-yyyy", null,
+            //    System.Globalization.DateTimeStyles.None, out dateOrg);
+            var isValidDate = DateTime.TryParseExact(order.OrderDateString, "mm-dd-yyyy", null, System.Globalization.DateTimeStyles.None, out dateOrg);
+            if (isValidDate)
+            {
+                order.OrderDate = dateOrg;
+            }
+
+            var isValidModel = TryUpdateModel(order);
+            if (isValidModel)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.OrderMasters.Add(order);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
