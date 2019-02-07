6bcf1f6ad1317f87863a9e089446f156568a713f
rjporosh <porosh199404232@gmail.com>
2019-02-08 01:53:20 +0600

    Lil Bit Edited Only Edit and Delete Not Working

--- AdvMasterDetails/Controllers/HomeController.cs
+++ AdvMasterDetails/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 using AdvMasterDetails.Models;
 
@@ -11,6 +12,7 @@ namespace AdvMasterDetails.Controllers
 {
     public class HomeController : Controller
     {
+        MyDatabaseEntities db = new MyDatabaseEntities();
         public ActionResult Index()
         {
             return View();
@@ -20,53 +22,23 @@ namespace AdvMasterDetails.Controllers
         public JsonResult GetProductCategories()
         {
             List<Category> categories;
-            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
             {
-                categories = dc.Categories.OrderBy(a => a.CategortyName).ToList();
+                categories = db.Categories.OrderBy(a => a.CategortyName).ToList();
             }
             return new JsonResult { Data = categories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
         public JsonResult GetProducts(int categoryID)
         {
             List<Product> products = new List<Product>();
-            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
             {
-                products = dc.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
+                products = db.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
             }
             return new JsonResult { Data = products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
         public ActionResult ShowAll()
         {
-            //String connnectionString = "data source=(LocalDB)\\MSSQLLocalDB;attachdbfilename=|DataDirectory|\\MyDatabase.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
-            //SqlConnection con = new SqlConnection(connnectionString);
-            //if (con.State == ConnectionState.Closed)
-            //{
-            //    con.Open();
-            //}
-            //String sql = "SELECT * FROM OrderMaster";
-            //SqlCommand cmd = new SqlCommand(sql, con);
-
-            //var model = new List<Orders>();
-            //using (SqlConnection conn = new SqlConnection(connnectionString))
-            //{
-            //    conn.Open();
-            //    SqlDataReader rdr = cmd.ExecuteReader();
-            //    while (rdr.Read())
-            //    {
-            //        var order = new Orders();
-            //        order.OrderId = (int) rdr["OrderId"];
-            //        order.OrderNo = (string) rdr["OrderNo"];
-            //        order.OrderDate = (DateTime) rdr["OrderDate"];
-            //        order.Description = rdr["Description"].ToString();
-
-
-            //        model.Add(order);
-            //    }
-            //    conn.Close();
-            //}
-            //con.Close();
-            //return View(model);
-            MyDatabaseEntities db = new MyDatabaseEntities();
             var result = (from c in db.OrderMasters
                 select new Orders()
                 {
@@ -82,8 +54,6 @@ namespace AdvMasterDetails.Controllers
         {
             bool status = false;
             DateTime dateOrg;
-            //var isValidDate = DateTime.TryParseExact(order.OrderDate.ToString(), "mm-dd-yyyy", null,
-            //    System.Globalization.DateTimeStyles.None, out dateOrg);
             var isValidDate = DateTime.TryParseExact(order.OrderDateString, "mm-dd-yyyy", null, System.Globalization.DateTimeStyles.None, out dateOrg);
             if (isValidDate)
             {
@@ -93,10 +63,10 @@ namespace AdvMasterDetails.Controllers
             var isValidModel = TryUpdateModel(order);
             if (isValidModel)
             {
-                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                using (MyDatabaseEntities db = new MyDatabaseEntities())
                 {
-                    dc.OrderMasters.Add(order);
-                    dc.SaveChanges();
+                    db.OrderMasters.Add(order);
+                    db.SaveChanges();
                     status = true;
                 }
             }
@@ -106,6 +76,19 @@ namespace AdvMasterDetails.Controllers
         public ActionResult Edit(int id)
         {
             throw new NotImplementedException();
+        }
+
+        public ActionResult Delete(OrderMaster id)
+        {
+            // List<OrderMaster> order = new List<OrderMaster>();
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
+            {
+                // order=db.OrderMasters.Where(a => a.OrderID.Equals(id)).OrderBy(a => a.OrderDetails).ToList();
+                db.OrderMasters.Remove(id);
+                db.SaveChanges();
+                //ShowAll();
+            }
+                throw new NotImplementedException();
         }
     }
 }
